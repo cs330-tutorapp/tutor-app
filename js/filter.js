3b05f1a98348044555f651f0f9110db04c9d400a
ccchristine <51844172+ccchristine@users.noreply.github.com>
2020-02-19 23:16:05 -0600

    update

--- js/filter.js
+++ js/filter.js
@@ -2,27 +2,23 @@ const filterTable = () => {
     const searchTerm = document.querySelector("#search").value;
     const table = document.querySelector("#myTable");
     const rows = document.querySelectorAll("tr");
+    console.log(searchTerm)
     for (const row of rows) {
-       const firstcell = row.querySelector("td");
-       if (!firstcell) {
+       var entry = row.querySelectorAll("td");
+       console.log(entry);
+       if (entry.length == 0) {
           continue;
        }
-       const isMatch = checkMatch(firstcell, searchTerm);
+       const isMatch = checkMatch(entry, searchTerm);
        if (isMatch) {
           row.classList.remove('hide');
        } else {
           row.classList.add('hide');
-       }
+       }      
     }
  };
-
+ 
  const checkMatch = (cell, searchTerm) => {
-<<<<<<< HEAD:filter.js
-      const name = cell.innerHTML;
-      if (name.toUpperCase().indexOf(searchTerm.toUpperCase()) > -1) {
-          return true;
-      }
-=======
     for(var i = 0; i < cell.length - 1; i++) {
         const name = cell[i].innerHTML; 
         if (name.toUpperCase().indexOf(searchTerm.toUpperCase()) > -1) {
@@ -30,8 +26,7 @@ const filterTable = () => {
         }
     }
     
->>>>>>> project_5:js/filter.js
     return false;
  };
-
+ 
  document.querySelector('#search').onkeyup = filterTable;
