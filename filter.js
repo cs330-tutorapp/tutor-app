51fc06c6403f61956b88cd0403313c30c11a4e2b
kathrynetao <61030954+kathrynetao@users.noreply.github.com>
2020-02-19 22:53:42 -0600

    Add files via upload

--- filter.js
+++ filter.js
@@ -2,31 +2,26 @@ const filterTable = () => {
     const searchTerm = document.querySelector("#search").value;
     const table = document.querySelector("#myTable");
     const rows = document.querySelectorAll("tr");
-    console.log(searchTerm)
     for (const row of rows) {
-       var entry = row.querySelectorAll("td");
-       console.log(entry);
-       if (entry.length == 0) {
+       const firstcell = row.querySelector("td");
+       if (!firstcell) {
           continue;
        }
-       const isMatch = checkMatch(entry, searchTerm);
+       const isMatch = checkMatch(firstcell, searchTerm);
        if (isMatch) {
           row.classList.remove('hide');
        } else {
           row.classList.add('hide');
-       }      
+       }
     }
  };
- 
+
  const checkMatch = (cell, searchTerm) => {
-    for(var i = 0; i < cell.length; i++) {
-        const name = cell[i].innerHTML; 
-        if (name.toUpperCase().indexOf(searchTerm.toUpperCase()) > -1) {
-            return true;
-        }
-    }
-    
+      const name = cell.innerHTML;
+      if (name.toUpperCase().indexOf(searchTerm.toUpperCase()) > -1) {
+          return true;
+      }
     return false;
  };
- 
+
  document.querySelector('#search').onkeyup = filterTable;
