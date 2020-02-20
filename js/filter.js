ed99271ac7b90f7da9a7a099e92522d5b83e675f
ccchristine <51844172+ccchristine@users.noreply.github.com>
2020-02-19 23:11:31 -0600

    update

--- js/filter.js
+++ js/filter.js
@@ -2,23 +2,27 @@ const filterTable = () => {
     const searchTerm = document.querySelector("#search").value;
     const table = document.querySelector("#myTable");
     const rows = document.querySelectorAll("tr");
-    console.log(searchTerm)
     for (const row of rows) {
-       var entry = row.querySelectorAll("td");
-       console.log(entry);
-       if (entry.length == 0) {
+       const firstcell = row.querySelector("td");
+       if (!firstcell) {
           continue;
        }
-       const isMatch = checkMatch(entry, searchTerm);
+       const isMatch = checkMatch(firstcell, searchTerm);
        if (isMatch) {
           row.classList.remove('hide');
        } else {
           row.classList.add('hide');
-       }      
+       }
     }
  };
- 
+
  const checkMatch = (cell, searchTerm) => {
+<<<<<<< HEAD:filter.js
+      const name = cell.innerHTML;
+      if (name.toUpperCase().indexOf(searchTerm.toUpperCase()) > -1) {
+          return true;
+      }
+=======
     for(var i = 0; i < cell.length - 1; i++) {
         const name = cell[i].innerHTML; 
         if (name.toUpperCase().indexOf(searchTerm.toUpperCase()) > -1) {
@@ -26,7 +30,8 @@ const filterTable = () => {
         }
     }
     
+>>>>>>> project_5:js/filter.js
     return false;
  };
- 
+
  document.querySelector('#search').onkeyup = filterTable;
