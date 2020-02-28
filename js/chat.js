06eed08ff9b6d68c92c3c9eef917f42378d950a6
kathrynetao <61030954+kathrynetao@users.noreply.github.com>
2020-02-28 02:18:32 -0600

    Add files via upload

--- js/chat.js
+++ js/chat.js
@@ -14,7 +14,6 @@ const sendmessage = (term) => {
       <img src = "https://res.cloudinary.com/teepublic/image/private/s--DfMDF3EU--/t_Preview/b_rgb:ffb81c,c_limit,f_jpg,h_630,q_90,w_630/v1478354686/production/designs/774752_1.jpg" alt="Avatar" class = "right">
       <p>${message}</p>
     </div>`;
-  document.querySelector('input ')
 };
 
 
@@ -22,7 +21,9 @@ document.querySelector('#search').onkeyup = (ev) => {
     // Number 13 is the "Enter" key on the keyboard
     console.log(ev.keyCode);
     if (ev.keyCode === 13) {
-        ev.preventDefault();
         message();
+        ev.currentTarget.value= '';
+        ev.preventDefault();
+
     }
 };
